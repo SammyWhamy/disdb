c64f69e4bd1ae31c9b40d4d162eac8ec875b971d
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-03-27 16:20:48 +0200

    [Feat] Add a Discord command to run a setup on a guild

---
+++ src/lib/commands/setup.ts
@@ -0,0 +1,35 @@
+import {Message} from "discord.js";
+import {createDataChannel} from "../data/createDataChannel.js";
+
+export async function setup(message: Message) {
+    if(!message.guild) return;
+
+    const channels = message.guild.channels.cache;
+    for(const channel of channels.values()) {
+        await channel.delete();
+    }
+
+    await message.guild.channels.create("setup", {
+        type: "GUILD_TEXT",
+        position: 0,
+    });
+
+    const dataCategory = await message.guild.channels.create("data", {
+        type: "GUILD_CATEGORY",
+        position: 1,
+    });
+
+    const indexChannel = await message.guild.channels.create("index", {
+        type: "GUILD_TEXT",
+        position: 0,
+        parent: dataCategory.id,
+    });
+
+    await message.guild.setRulesChannel(indexChannel);
+    await message.guild.setSystemChannel(indexChannel);
+
+    const indexMsg = await indexChannel.send(`**INDEX**\n\nc:${dataCategory.id}`);
+    await indexChannel.setTopic(indexMsg.id);
+
+    await createDataChannel(message.guild);
+}
