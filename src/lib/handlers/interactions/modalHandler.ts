813cb2b9fd751dd9f92cf05cbd3b8d40d72cff6e
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-04-10 16:38:34 +0200

    [Fix] Handle errors on control panel

--- src/lib/handlers/interactions/modalHandler.ts
+++ src/lib/handlers/interactions/modalHandler.ts
@@ -5,14 +5,24 @@ export async function modalHandler(this: DiscordManager, modal: ModalSubmitInter
     switch(modal.customId) {
         case "get": {
             const key = modal.fields.getTextInputValue("key")
-            const value = await this.get(key);
 
-            const replyEmbed = new EmbedBuilder()
-                .setTitle(`Result for key: ${key}`)
-                .setDescription(value ?? "Value not found in database.")
-                .setColor(8560895);
+            try {
+                const value = await this.get(key);
 
-            await modal.reply({embeds: [replyEmbed], ephemeral: true});
+                const replyEmbed = new EmbedBuilder()
+                    .setTitle(`Result for key: ${key}`)
+                    .setDescription(value ?? "Value not found in database.")
+                    .setColor(8560895);
+
+                await modal.reply({embeds: [replyEmbed], ephemeral: true});
+            } catch(e: any) {
+                const replyEmbed = new EmbedBuilder()
+                    .setTitle(`Error getting key: ${key}`)
+                    .setDescription(e.message)
+                    .setColor(16711680);
+
+                await modal.reply({embeds: [replyEmbed], ephemeral: true});
+            }
 
             break;
         }
@@ -21,14 +31,23 @@ export async function modalHandler(this: DiscordManager, modal: ModalSubmitInter
             const key = modal.fields.getTextInputValue("key");
             const value = modal.fields.getTextInputValue("value");
 
-            await this.set(key, value);
+            try {
+                await this.set(key, value);
 
-            const replyEmbed = new EmbedBuilder()
-                .setTitle(`Set key: ${key}`)
-                .setDescription(`Value: ${value}`)
-                .setColor(8560895);
+                const replyEmbed = new EmbedBuilder()
+                    .setTitle(`Set key: ${key}`)
+                    .setDescription(`Value: ${value}`)
+                    .setColor(8560895);
 
-            await modal.reply({embeds: [replyEmbed], ephemeral: true});
+                await modal.reply({embeds: [replyEmbed], ephemeral: true});
+            } catch(e: any) {
+                const replyEmbed = new EmbedBuilder()
+                    .setTitle(`Error setting key: ${key}`)
+                    .setDescription(e.message)
+                    .setColor(16711680);
+
+                await modal.reply({embeds: [replyEmbed], ephemeral: true});
+            }
 
             break;
         }
@@ -59,12 +78,21 @@ export async function modalHandler(this: DiscordManager, modal: ModalSubmitInter
         case "exists": {
             const key = modal.fields.getTextInputValue("key");
 
-            const replyEmbed = new EmbedBuilder()
-                .setTitle(`Key: ${key}`)
-                .setDescription(await this.exists(key) ? "Key exists" : "Key does not exist")
-                .setColor(8560895);
+            try {
+                const replyEmbed = new EmbedBuilder()
+                    .setTitle(`Key: ${key}`)
+                    .setDescription(await this.exists(key) ? "Key exists" : "Key does not exist")
+                    .setColor(8560895);
 
-            await modal.reply({embeds: [replyEmbed], ephemeral: true});
+                await modal.reply({embeds: [replyEmbed], ephemeral: true});
+            } catch(e: any) {
+                const replyEmbed = new EmbedBuilder()
+                    .setTitle(`Error checking key: ${key}`)
+                    .setDescription(e.message)
+                    .setColor(16711680);
+
+                await modal.reply({embeds: [replyEmbed], ephemeral: true});
+            }
 
             break;
         }
