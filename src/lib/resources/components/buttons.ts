88e32a372f4413f6a2a4c41c517dac4b5e126d1d
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-04-12 01:50:00 +0200

    [Fix] Add component type to ControlPanelButtons

--- src/lib/resources/components/buttons.ts
+++ src/lib/resources/components/buttons.ts
@@ -6,17 +6,21 @@ export const ControlPanelButtons: ActionRowData<ButtonComponentData> & Required<
         customId: "get",
         label: "Get",
         style: ButtonStyle.Primary,
+        type: ComponentType.Button,
     }, {
         customId: "set",
         label: "Set",
         style: ButtonStyle.Success,
+        type: ComponentType.Button,
     }, {
         customId: "delete",
         label: "Delete",
         style: ButtonStyle.Danger,
+        type: ComponentType.Button,
     }, {
         customId: "exists",
         label: "Exists",
         style: ButtonStyle.Secondary,
+        type: ComponentType.Button,
     }],
 };
